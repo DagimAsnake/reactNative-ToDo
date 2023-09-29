b9f7387f6db3f1f8b0bff620d830a0dc3e113f35
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-29 05:55:53 -1200

    handle list todos

--- Screens/ListToDoScreen.js
+++ Screens/ListToDoScreen.js
@@ -1,74 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, FlatList, Pressable } from 'react-native';
 import { CheckBox } from 'react-native-elements';
-import { useNavigation } from "@react-navigation/native"
-
-const ToDo = [
-  {
-    id: '1',
-    title: 'first',
-    description: 'first first first first first first first',
-    checked: false,
-  },
-  {
-    id: '2',
-    title: 'second',
-    description: 'second second second second second second second',
-    checked: false,
-  },
-  {
-    id: '3',
-    title: 'fourth',
-    description: 'fourth fourth fourth fourth fourth fourth fourth',
-    checked: false,
-  },
-  {
-    id: '4',
-    title: "third",
-    description: "third third third third third third third",
-    checked: false,
-  },
-  {
-    id: '5',
-    title: "fifth",
-    description: "fifth fifth fifth fifth fifth fifth fifth",
-    checked: false,
-  },
-  {
-    id: '6',
-    title: "six",
-    description: "six six six six six six six",
-    checked: false,
-  },
-];
+import { useNavigation, useIsFocused } from '@react-navigation/native';
 
 const ListToDoScreen = () => {
-  const navigation = useNavigation()
-  const [toDoItems, setToDoItems] = useState(ToDo);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+  const [toDoItems, setToDoItems] = useState([]);
+
+  useEffect(() => {
+    if (isFocused) {
+      fetchTodos();
+    }
+  }, [isFocused]);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('http://192.168.43.142:8080/todo');
+      const data = await response.json();
+      setToDoItems(data.todos);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
 
   const renderToDo = ({ item }) => {
     const handlePress = () => {
-      navigation.navigate("Detail ToDo")
+      navigation.navigate('Detail ToDo');
     };
 
     const handleCheckBox = () => {
-      const updatedItems = toDoItems.map((todo) => {
-        if (todo.id === item.id) {
-          return { ...todo, checked: !todo.checked };
-        }
-        return todo;
-      });
-      setToDoItems(updatedItems);
+      // Handle checkbox logic here
     };
 
     return (
       <Pressable onPress={handlePress}>
         <View className="flex-row p-4 items-center border-blue-500 border-b mx-3">
           <Text className="flex-1 text-lg text-blue-500">{item.title}</Text>
-          <CheckBox
-            checked={item.checked}
-            onPress={handleCheckBox}
-          />
+          <CheckBox checked={item.checked} onPress={handleCheckBox} />
         </View>
       </Pressable>
     );
@@ -76,7 +45,7 @@ const ListToDoScreen = () => {
 
   return (
     <View className="flex-1">
-      <FlatList data={toDoItems} keyExtractor={(item) => item.id} renderItem={renderToDo} />
+      <FlatList data={toDoItems} keyExtractor={(item) => item._id} renderItem={renderToDo} />
     </View>
   );
 };
