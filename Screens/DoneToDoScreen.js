d9293395483512015a7f54fe342880f6fb5acb03
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-28 23:53:00 -1200

    added done

--- Screens/DoneToDoScreen.js
+++ Screens/DoneToDoScreen.js
@@ -1,10 +1,64 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react';
+import { Text, View, FlatList, Pressable } from 'react-native';
+import { CheckBox } from 'react-native-elements';
+
+const ToDo = [
+  {
+    id: '1',
+    title: 'first',
+    description: 'first first first first first first first',
+    checked: true,
+  },
+  {
+    id: '2',
+    title: 'second',
+    description: 'second second second second second second second',
+    checked: true,
+  },
+  {
+    id: '3',
+    title: 'fourth',
+    description: 'fourth fourth fourth fourth fourth fourth fourth',
+    checked: true,
+  },
+];
 
 const DoneToDoScreen = () => {
-  return (
-    <Text className='text-green-600'>DoneToDoScreen</Text>
-  )
-}
+  const [toDoItems, setToDoItems] = useState(ToDo);
 
-export default DoneToDoScreen
+  const renderToDo = ({ item }) => {
+    const handlePress = () => {
+      // Handle press event for the card
+    };
+
+    const handleCheckBox = () => {
+      const updatedItems = toDoItems.map((todo) => {
+        if (todo.id === item.id) {
+          return { ...todo, checked: !todo.checked };
+        }
+        return todo;
+      });
+      setToDoItems(updatedItems);
+    };
+
+    return (
+      <Pressable onPress={handlePress}>
+        <View className="flex-row p-4 items-center border-b">
+          <Text className="flex-1 text-lg">{item.title}</Text>
+          <CheckBox
+            checked={item.checked} 
+            onPress={ handleCheckBox}
+          />
+        </View>
+      </Pressable>
+    );
+  };
+
+  return (
+    <View className="flex-1">
+      <FlatList data={toDoItems} keyExtractor={(item) => item.id} renderItem={renderToDo} />
+    </View>
+  );
+};
+
+export default DoneToDoScreen;
--- Screens/ListToDoScreen.js
+++ Screens/ListToDoScreen.js
@@ -1,52 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Text, View, FlatList, Pressable } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import { CheckBox } from 'react-native-elements';
 
 const ToDo = [
   {
     id: '1',
-    title: "first",
-    description: "first first first first first first first"
+    title: 'first',
+    description: 'first first first first first first first',
+    checked: false,
   },
   {
     id: '2',
-    title: "second",
-    description: "second second second second second second second"
+    title: 'second',
+    description: 'second second second second second second second',
+    checked: false,
   },
   {
     id: '3',
-    title: "fourth",
-    description: "fourth fourth fourth fourth fourth fourth fourth"
+    title: 'fourth',
+    description: 'fourth fourth fourth fourth fourth fourth fourth',
+    checked: false,
   },
   {
     id: '4',
     title: "third",
-    description: "third third third third third third third"
+    description: "third third third third third third third",
+    checked: false,
   },
   {
     id: '5',
     title: "fifth",
-    description: "fifth fifth fifth fifth fifth fifth fifth"
+    description: "fifth fifth fifth fifth fifth fifth fifth",
+    checked: false,
   },
   {
     id: '6',
     title: "six",
-    description: "six six six six six six six"
+    description: "six six six six six six six",
+    checked: false,
   },
-  
-]
+];
 
 const ListToDoScreen = () => {
+  const [toDoItems, setToDoItems] = useState(ToDo);
+
   const renderToDo = ({ item }) => {
     const handlePress = () => {
       // Handle press event for the card
+    };
+
+    const handleCheckBox = () => {
+      const updatedItems = toDoItems.map((todo) => {
+        if (todo.id === item.id) {
+          return { ...todo, checked: !todo.checked };
+        }
+        return todo;
+      });
+      setToDoItems(updatedItems);
     };
 
     return (
       <Pressable onPress={handlePress}>
         <View className="flex-row p-4 items-center border-b">
           <Text className="flex-1 text-lg">{item.title}</Text>
-          <Feather name="check-square" size={24} className="text-gray-500" />
+          <CheckBox
+            checked={item.checked}
+            onPress={handleCheckBox}
+          />
         </View>
       </Pressable>
     );
@@ -54,7 +74,7 @@ const ListToDoScreen = () => {
 
   return (
     <View className="flex-1">
-      <FlatList data={ToDo} keyExtractor={(item) => item.id} renderItem={renderToDo} />
+      <FlatList data={toDoItems} keyExtractor={(item) => item.id} renderItem={renderToDo} />
     </View>
   );
 };
